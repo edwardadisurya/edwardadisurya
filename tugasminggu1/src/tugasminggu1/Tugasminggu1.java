bf8ae63a5bd70261bc47b909832c7ec4f07bd9c2
edwardadisurya <125243618+edwardadisurya@users.noreply.github.com>
2023-02-13 15:00:28 +0700

    Add files via upload

---
+++ tugasminggu1/src/tugasminggu1/Tugasminggu1.java
@@ -0,0 +1,17 @@
+
+package tugasminggu1;
+
+/**
+ *
+ * @author User
+ */
+public class Tugasminggu1 {
+
+    public static void main(String[] args) {
+        System.out.println("Edward Aqilah Adi Surya");
+        System.out.println("E4122800");
+        System.out.println("`golongan B");
+        
+    }
+    
+}
